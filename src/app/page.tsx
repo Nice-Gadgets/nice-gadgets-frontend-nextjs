23f9dc946d8b09a3c865b3a00bf4a1cebd64a40f
Михайло <mykhailo@MacBook-Air-Mihajlo.local>
2026-06-06 23:41:04 +0300

    refactor: draft version of dropdown component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 'use client';
-import { useState } from 'react';
-
-import { DropdownOption, SharedDropdown } from '@/shared/ui/dropdown';
 
 export default function Home() {
   return <main className="bg-brand-primary">Hello World</main>;
