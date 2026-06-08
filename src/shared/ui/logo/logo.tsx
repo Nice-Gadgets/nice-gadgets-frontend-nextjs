b6dc3476c73575794862f74600df353bd902098e
Tymur <timurradkevic84@mail.com>
2026-06-08 16:47:06 +0300

    feat: add shared logo link component

--- src/shared/ui/logo/logo.tsx
+++ src/shared/ui/logo/logo.tsx
@@ -1,5 +1,17 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Logo = () => {
-  return <Image src="/logo.png" alt="Logo" width={89} height={32} />;
+  return (
+    <Link href="/" className="inline-block">
+      <Image
+        src="/Logo.png"
+        alt="Logo"
+        width={96}
+        height={48}
+        className="w-24 h-12 lg:w-32 lg:h-16"
+        priority
+      />
+    </Link>
+  );
 };
