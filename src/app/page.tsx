976f8bdb85cbce1f587b23bd95a35ae367cd51e0
Михайло <mykhailo@MacBook-Air-Mihajlo.local>
2026-06-09 22:56:05 +0300

    feat: temporary commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+'use client';
+import { useState } from 'react';
+
+import { AppSelect } from '@/shared/ui/AppSelect';
+import { ProductCard } from '@/widgets/ProductCard';
+
+const sortOptions = [
+  { value: 'popular', label: 'popular' },
+  { value: 'price_asc', label: 'price_asc' },
+  { value: 'price_desc', label: 'price_desc' },
+  { value: 'newest', label: 'newest' },
+];
+
 export default function Home() {
-  return <main className="bg-brand-accent">Hello World</main>;
+  const [sortBy, setSortBy] = useState<string>('newest');
+  return (
+    <main className="bg-brand-white">
+      Hello World
+      <div className="p-4 bg-brand-green">
+        <AppSelect
+          label="filter by"
+          options={sortOptions}
+          value={sortBy}
+          onChange={(newValue) => setSortBy(newValue)}
+          className="w-44"
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-4 xl:max-w-7xl xl:mx-auto">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </main>
+  );
 }
--- src/widgets/ProductCard/ProductCard.tsx
+++ src/widgets/ProductCard/ProductCard.tsx
@@ -1,5 +1,3 @@
-import './ProductCard.css';
-
 import Image from 'next/image';
 
 import { Button } from '@/shared/ui/button';
@@ -8,60 +6,46 @@ import { BodyText, H3, SmallText, UppercaseText } from '@/shared/ui/Typography';
 
 export function ProductCard() {
   return (
-    <div className="product-card">
-      <div className="product-card__image-container">
+    <div className="box-border w-full h-full max-w-[288px] max-h-110 bg-brand-surface-1 p-8 flex flex-col justify-between select-none gap-2 min-[1200px]:max-w-68 min-[1200px]:h-126.5 min-[1200px]:max-h-126.5">
+      <div className="w-full h-49 flex items-center justify-center m-0 overflow-hidden">
         <Image
-          className="product-card__image"
+          className="max-h-full object-contain transition-transform duration-300 ease-in-out"
           src="/img/phones/apple-iphone-11/white/00.webp"
           alt={'Apple iPhone Xs 64GB Silver'}
-          width={300}
-          height={300}
+          width={250}
+          height={250}
         />
       </div>
 
-      <h3 className="product-card__title">
-        <BodyText>Apple iPhone Xs 64GB Silver (iMT9G2FS/A)</BodyText>
-      </h3>
+      <BodyText className="text-brand-white mt-2 line-clamp-2 min-h-10.5">
+        Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
+      </BodyText>
 
-      <div className="product-card__price-block">
-        <span className="product-card__price-actual">
-          <H3>$799</H3>
-        </span>
-        <span className="product-card__price-old">
-          <H3>$899</H3>
-        </span>
+      <div className="flex items-center gap-2 mt-1">
+        <H3 className="text-brand-white">$799</H3>
+        <H3 className="text-brand-secondary line-through">$899</H3>
       </div>
 
-      <div className="product-card__divider" />
+      <div className="w-full h-px bg-brand-elements" />
 
-      <div className="product-card__specs">
-        <div className="product-card__spec-row">
-          <span className="product-card__spec-label">
-            <SmallText>Screen</SmallText>
-          </span>
-          <span className="product-card__spec-value">
-            <UppercaseText>{'5.8" OLED'}</UppercaseText>
-          </span>
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <SmallText className="text-brand-secondary">Screen</SmallText>
+          <UppercaseText className="text-brand-white">
+            {'5.8" OLED'}
+          </UppercaseText>
         </div>
-        <div className="product-card__spec-row">
-          <span className="product-card__spec-label">
-            <SmallText>Capacity</SmallText>
-          </span>
-          <span className="product-card__spec-value">
-            <UppercaseText>64 GB</UppercaseText>
-          </span>
+        <div className="flex justify-between">
+          <SmallText className="text-brand-secondary">Capacity</SmallText>
+          <UppercaseText className="text-brand-white">64 GB</UppercaseText>
         </div>
-        <div className="product-card__spec-row">
-          <span className="product-card__spec-label">
-            <SmallText>RAM</SmallText>
-          </span>
-          <span className="product-card__spec-value">
-            <UppercaseText>4 GB</UppercaseText>
-          </span>
+        <div className="flex justify-between">
+          <SmallText className="text-brand-secondary">RAM</SmallText>
+          <UppercaseText className="text-brand-white">4 GB</UppercaseText>
         </div>
       </div>
 
-      <div className="product-card__actions">
+      <div className="flex gap-2 h-12">
         <Button variant="primary" className="h-10 grow cursor-pointer">
           Add to cart
         </Button>
