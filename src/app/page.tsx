8e7e990a82676af73ec97ae46396e37b434262c7
sergeisvidlo4 <sergeisvidlo4@gmail.com>
2026-06-12 09:58:00 +0300

    feat: add shop by category section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,44 @@
+import { ShopByCategory } from '@/widgets/ShopByCategory';
+
+import productsData from '../../public/api/products.json';
+
 export default function HomePage() {
-  return <main className="bg-brand-accent">Hello World</main>;
+  const phonesCount = productsData.filter(
+    (product) => product.category === 'phones',
+  ).length;
+
+  const tabletsCount = productsData.filter(
+    (product) => product.category === 'tablets',
+  ).length;
+
+  const accessoriesCount = productsData.filter(
+    (product) => product.category === 'accessories',
+  ).length;
+
+  const categories = [
+    {
+      title: 'Mobile phones',
+      href: '/phones',
+      imageSrc: '/img/category-phones.png',
+      modelsCount: phonesCount,
+    },
+    {
+      title: 'Tablets',
+      href: '/tablets',
+      imageSrc: '/img/category-tablets.png',
+      modelsCount: tabletsCount,
+    },
+    {
+      title: 'Accessories',
+      href: '/accessories',
+      imageSrc: '/img/category-accessories.png',
+      modelsCount: accessoriesCount,
+    },
+  ];
+
+  return (
+    <main className="mx-auto max-w-300 bg-brand-black px-4 py-6 sm:px-6 lg:px-8">
+      <ShopByCategory categories={categories} />
+    </main>
+  );
 }
